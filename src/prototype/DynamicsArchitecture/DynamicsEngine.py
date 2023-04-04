afc1f244420ad7b1780f6ca8be39eb0fdfedb324
João Vaz Carneiro <joao.carneiro@colorado.edu>
2023-04-04 08:40:21 -0600

    [squash]

--- src/prototype/DynamicsArchitecture/DynamicsEngine.py
+++ src/prototype/DynamicsArchitecture/DynamicsEngine.py
@@ -1,12 +1,18 @@
 class DynamicsEngine:
     def __init__(self):
-        return
+        self.systemList = []
 
     def categorizeSystem(self):
         return
 
     def propagateStates(self):
-        return
+        # NEED TO FIGURE OUT SYSTEM INTERACTION
+
+        # Loop through all systems
+        for system in self.systemList:
+            system.propagateDynamics()
+
+        # where to update kinematics?
 
     def setEnvironment(self):
         return
--- src/prototype/DynamicsArchitecture/System.py
+++ src/prototype/DynamicsArchitecture/System.py
@@ -6,3 +6,13 @@ class System:
         self.rDot_CN = None
         self.r_CN = None
         self.rDot_CB = None
+
+        self.base = None
+        self.subsytemList = []
+
+    def propagateDynamics(self):
+        for subsystem in self.subsytemList:
+            subsystem.updateEffectorMassProps()
+            subsystem.computeDerivatives()
+            subsystem.updateContributions()
+            subsystem.updateEnergyMomContributions()
