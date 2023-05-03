b8d5f2dcbfa88954a26f98ac48df57e62a9ec5e4
Leah Kiner <leki4360@colorado.edu>
2023-05-03 14:08:21 -0600

    Adding actuator class skeleton

---
+++ src/simulation/dynamics/_GeneralModuleFiles/Actuator.h
@@ -0,0 +1,44 @@
+/*
+ ISC License
+
+ Copyright (c) 2023, Autonomous Vehicle Systems Lab, University of Colorado at Boulder
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+ */
+
+#ifndef ACTUATOR_H
+#define ACTUATOR_H
+
+#include "architecture/_GeneralModuleFiles/sys_model.h"
+#include "architecture/utilities/bskLogging.h"
+#include "architecture/messaging/messaging.h"
+
+#include "simulation/dynamics/_GeneralModuleFiles/Frame.h"
+#include "simulation/dynamics/_GeneralModuleFiles/Vector.h"
+
+#include <Eigen/Core>
+
+/*! @brief basic Basilisk C++ module class */
+class Actuator {
+public:
+    Actuator() = default;
+    ~Actuator() = default;
+
+    Frame* frame = nullptr;
+    Vector Force;
+    Vector Torque;
+};
+
+
+#endif
