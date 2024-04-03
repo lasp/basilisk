d42ff43759ec872bfad87143de02c91978337e99
patrick kenneally <patrick.kenneally@lasp.colorado.edu>
2024-04-03 13:52:02 -0600

    Test commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -95,11 +95,11 @@ html_theme = "sphinx_rtd_theme"
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    'style_nav_header_background': '#CFB87C'
+    # 'style_nav_header_background': '#CFB87C'
 }
 
 html_context = {
-    'css_files': ['_static/css/custom.css']
+    # 'css_files': ['_static/css/custom.css']
 }
 
 html_logo = "./_images/static/Basilisk-Logo.png"
