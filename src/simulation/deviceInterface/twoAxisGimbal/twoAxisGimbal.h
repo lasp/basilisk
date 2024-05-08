4b06c67ffd543622ad848463e4c27062b4005d1d
Leah Kiner <leahannkiner@gmail.com>
2024-05-08 16:54:20 -0600

    Add other module variables

--- src/simulation/deviceInterface/twoAxisGimbal/twoAxisGimbal.h
+++ src/simulation/deviceInterface/twoAxisGimbal/twoAxisGimbal.h
@@ -59,10 +59,50 @@ public:
     BSKLogger *bskLogger;                                                           //!< BSK Logging
 
 private:
+    // Stepper motor state data
     double motorStepAngle;                                                          //!< [rad] Angle the stepper motor moves through for a single step
     double motorStepTime;                                                           //!< [s] Time required for a single motor step (constant)
+    double motor1ThetaInit;                                                         //!< [rad] Initial stepper motor 1 angle
+    double motor2ThetaInit;                                                         //!< [rad] Initial stepper motor 2 angle
+    int motor1StepsCommanded;
+    int motor2StepsCommanded;
+    double motor1ThetaRef;
+    double motor2ThetaRef;
+
+    // Gimbal state data
+    Eigen::Vector3d gimbalPRV_F0M;
+    Eigen::Vector3d gimbalPRVRotHat;                                                //!< Gimbal prv Eigen axis of rotation
     Eigen::Vector3d gimbalRotHat1_M;                                                //!< Gimbal tip angle axis of rotation 1 expressed in M frame components
     Eigen::Vector3d gimbalRotHat2_F;                                                //!< Gimbal tilt angle axis of rotation 2 expressed in F frame components
+    int gimbalStepCount;
+    int gimbalStepsCommanded;
+    double gimbalStepAngle;
+    double gimbalPRVThetaDDotMax;                                                   //!< [rad/s^2] Current profiled gimbal prv angular acceleration
+    double gimbalPRVThetaRef;
+    double intermediateGimbalPRVThetaInit;
+    double intermediateGimbalPRVThetaRef;
+    double gimbalPRVTheta;                                                          //!< [rad] Current profiled gimbal prv angle
+    double gimbalPRVThetaDot;                                                       //!< [rad/s] Current profiled gimbal prv angle rate
+    double gimbalPRVThetaDDot;                                                      //!< [rad/s^2] Current profiled gimbal prv angular acceleration
+    double gimbalTheta1;                                                            //!< [rad] Current gimbal tip angle
+    double gimbalTheta2;                                                            //!< [rad] Current gimbal tilt angle
+
+    /* Temporal parameters */
+    double previousWrittenTime;                                                     //!< [ns] Time the last input message was written
+    double tInit;                                                                   //!< [s] Simulation time at the beginning of the maneuver
+    double ts;                                                                      //!< [s] The simulation time halfway through the maneuver (switch time for ang accel)
+    double tf;                                                                      //!< [s] Simulation time when the maneuver is finished
+
+    /* Boolean parameters */
+    bool segment1Complete;
+    bool segment2Complete;
+    bool completion;                                                                //!< Boolean designating a fully completed maneuver
+    bool gimbalStepComplete;                                                        //!< Boolean designating a completed step
+    bool newMsg;                                                                    //!< Boolean designating a new message was written
+
+    /* Constant parameters */
+    double a;                                                                       //!< Parabolic constant for the first half of a step
+    double b;                                                                       //!< Parabolic constant for the second half of a step
 };
 
 #endif
