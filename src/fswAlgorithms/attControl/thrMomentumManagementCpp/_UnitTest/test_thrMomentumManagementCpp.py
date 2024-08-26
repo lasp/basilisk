f8ae2681e1d866277db009e028f89a7b7f612498
João Vaz Carneiro <joao.carneiro@colorado.edu>
2024-08-26 14:05:39 +0100

    Add test files

---
+++ src/fswAlgorithms/attControl/thrMomentumManagementCpp/_UnitTest/test_thrMomentumManagementCpp.py
@@ -0,0 +1,85 @@
+#
+#  ISC License
+#
+#  Copyright (c) 2024, Laboratory of Atmospheric and Space Physics, University of Colorado at Boulder
+#
+#  Permission to use, copy, modify, and/or distribute this software for any
+#  purpose with or without fee is hereby granted, provided that the above
+#  copyright notice and this permission notice appear in all copies.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+#  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+#  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+#  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+#  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+#  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+#  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+import inspect
+import os
+
+import pytest
+import numpy as np
+
+filename = inspect.getframeinfo(inspect.currentframe()).filename
+path = os.path.dirname(os.path.abspath(filename))
+
+# Import all of the modules that we are going to be called in this simulation
+from Basilisk.utilities import SimulationBaseClass, macros, fswSetupRW
+from Basilisk.fswAlgorithms import thrMomentumManagementCpp
+from Basilisk.architecture import messaging
+
+
+@pytest.mark.parametrize("hsMinCheck", [(0), (1)])
+def test_thrMomentumManagement(show_plots, hsMinCheck):
+    unitTestSim = SimulationBaseClass.SimBaseClass()
+    unitTaskName = "unitTask"
+    unitProcessName = "TestProcess"
+    testProcessRate = macros.sec2nano(0.5)
+    testProc = unitTestSim.CreateNewProcess(unitProcessName)
+    testProc.addTask(unitTestSim.CreateNewTask(unitTaskName, testProcessRate))
+
+    module = thrMomentumManagementCpp.ThrMomentumManagementCpp()
+    module.ModelTag = "thrMomentumManagement"
+    unitTestSim.AddModelToTask(unitTaskName, module)
+
+    if hsMinCheck:
+        module.hs_min = 1000. / 6000. * 100.  # Nms
+    else:
+        module.hs_min = 100. / 6000. * 100.  # Nms
+
+    # Define instantaneous wheel speed information to run the simulation
+    rwSpeedMessage = messaging.RWSpeedMsgPayload()
+    rwSpeedMessage.wheelSpeeds = [10.0, -25.0, 50.0, 100.]
+    rwSpeedInMsg = messaging.RWSpeedMsg().write(rwSpeedMessage)
+    fswSetupRW.clearSetup()
+    Js = 0.1
+    fswSetupRW.create([1.0, 0.0, 0.0], Js)
+    fswSetupRW.create([0.0, 1.0, 0.0], Js)
+    fswSetupRW.create([0.0, 0.0, 1.0], Js)
+    fswSetupRW.create([0.5773502691896258, 0.5773502691896258, 0.5773502691896258], Js)
+    rwConfigInMsg = fswSetupRW.writeConfigMessage()
+    module.rwSpeedsInMsg.subscribeTo(rwSpeedInMsg)
+    module.rwConfigDataInMsg.subscribeTo(rwConfigInMsg)
+
+    dataLog = module.deltaHOutMsg.recorder()
+    unitTestSim.AddModelToTask(unitTaskName, dataLog)
+
+    unitTestSim.ConfigureStopTime(macros.sec2nano(0.5))
+    unitTestSim.InitializeSimulation()
+    unitTestSim.ExecuteSimulation()
+
+    # Define expected output depending on the type of check
+    if hsMinCheck == 1:
+        trueVector = np.array([[0.0, 0.0, 0.0]] * 2)
+    else:
+        trueVector = np.array([[-5.914369484146579, -2.858300248464629, -9.407020039211664]] * 2)
+    np.testing.assert_allclose(trueVector, dataLog.torqueRequestBody, rtol=1e-12)
+
+
+if __name__ == "__main__":
+    test_thrMomentumManagement(
+                 True,
+                 0            # hsMinCheck
+               )
