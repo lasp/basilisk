d3c5f20ec9dd9896241553a58accd2d362ad926b
Bryson Smith <brsm3129@colorado.edu>
2024-08-28 12:12:14 -0600

    Add TickerModule

---
+++ src/utilities/tests/TickerModule.py
@@ -0,0 +1,17 @@
+from Basilisk.architecture import sysModel
+class TickerModule(sysModel.SysModel):
+
+    def __init__(self):
+        super(TickerModule, self).__init__()
+
+        self._ticker = 0
+        self.moduleTag = "testModule"
+
+    def UpdateState(self, current_sim_nanos):
+        self._ticker += 1
+
+    def GetTicker(self):
+        return self._ticker
+
+    def Reset(self, current_sim_nanos):
+        pass
