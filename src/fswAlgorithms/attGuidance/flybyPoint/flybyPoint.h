8a241b5e5cb6fa67662f15f85e4b01ed1c7a994f
Thibaud Teil <thibaud.teil@colorado.edu>
2024-09-19 14:21:28 -0600

    reformat module into methods

--- src/fswAlgorithms/attGuidance/flybyPoint/flybyPoint.h
+++ src/fswAlgorithms/attGuidance/flybyPoint/flybyPoint.h
@@ -34,6 +34,16 @@ public:
     void Reset(uint64_t CurrentSimNanos) override;
     void UpdateState(uint64_t CurrentSimNanos) override;
 
+    std::tuple<Eigen::Vector3d, Eigen::Vector3d> readRelativeState();
+    bool checkValidity(Eigen::Vector3d &r_BN_N, Eigen::Vector3d &v_BN_N) const;
+    void computeFlybyParameters(Eigen::Vector3d &r_BN_N, Eigen::Vector3d &v_BN_N);
+    void computeRN(Eigen::Vector3d &r_BN_N, Eigen::Vector3d &v_BN_N);
+    std::tuple<Eigen::Vector3d, Eigen::Vector3d, Eigen::Vector3d> computeGuidanceSolution() const;
+    void writeMessages(uint64_t currentSimNanos,
+                       Eigen::Vector3d &sigma_RN,
+                       Eigen::Vector3d &omega_RN_N,
+                       Eigen::Vector3d &omegaDot_RN_N);
+
     double getTimeBetweenFilterData() const;
     void setTimeBetweenFilterData(double timeBetweenFilterData);
     double getToleranceForCollinearity() const;
@@ -46,6 +56,7 @@ public:
     Message<AttRefMsgPayload> attRefOutMsg;                     //!< Attitude reference output message
 
 private:
+    double dt = 0; //!< current time step between last two updates
     double timeBetweenFilterData = 0;       //!< time between two subsequent reads of the filter information
     double toleranceForCollinearity = 0;            //!< tolerance for singular conditions when position and velocity are collinear
     int signOfOrbitNormalFrameVector = 1;  //!< Sign of orbit normal vector to complete reference frame
@@ -55,8 +66,7 @@ private:
     double gamma0 = 0;              //!< flight path angle of the spacecraft at time of read [rad]
     uint64_t lastFilterReadTime = 0;  //!< time of last filter read
     Eigen::Matrix3d R0N;           //!< inertial-to-reference DCM at time of read
-    Eigen::Vector3d r_BN_N;           //!< filter spacecraft position estimate in inertial coordinates
-    Eigen::Vector3d v_BN_N;           //!< filter spacecraft velocity estimate in inertial coordinates
+
 };
 
 
