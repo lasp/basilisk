460082c101bf4aa7d345b9bc0e74830367c11576
Jonathan Castello <twisolar@gmail.com>
2024-09-25 11:54:38 -0700

    Initial (working?) attempt at using vcpkg

--- src/cmake/usingOpenCV.cmake
+++ src/cmake/usingOpenCV.cmake
@@ -1,30 +1,5 @@
 find_package(OpenCV REQUIRED)
-if (OpenCV_FOUND)
-  set_property(TARGET opencv::opencv PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${DIRECTORIES})
-  target_link_libraries(${TARGET_NAME} PRIVATE opencv::opencv)
-
-  # The following code is added to work around conan issues with Xcode not seeing the release version in the IDE and
-  # giving false errors
-  get_property(
-    INCLUDE_DIRECTORY
-    TARGET opencv::opencv
-    PROPERTY INTERFACE_INCLUDE_DIRECTORIES)
-  if(DEFINED opencv_INCLUDE_DIRS_DEBUG)
-    set(DIRECTORIES
-            "$<$<CONFIG:Debug>:${opencv_INCLUDE_DIRS_DEBUG}>"
-            "$<$<CONFIG:Release>:${opencv_INCLUDE_DIRS_DEBUG}>"
-            "$<$<CONFIG:RelWithDebInfo>:${opencv_INCLUDE_DIRS_DEBUG}>"
-            "$<$<CONFIG:MinSizeRel>:${opencv_INCLUDE_DIRS_DEBUG}>")
-  else()
-    set(DIRECTORIES
-            "$<$<CONFIG:Debug>:${opencv_INCLUDE_DIRS_RELEASE}>"
-            "$<$<CONFIG:Release>:${opencv_INCLUDE_DIRS_RELEASE}>"
-            "$<$<CONFIG:RelWithDebInfo>:${opencv_INCLUDE_DIRS_RELEASE}>"
-            "$<$<CONFIG:MinSizeRel>:${opencv_INCLUDE_DIRS_RELEASE}>")
-  endif()
-else ()
-  message(FATAL_ERROR "opencv not found")
-endif ()
+target_link_libraries(${TARGET_NAME} PRIVATE opencv_core opencv_dnn opencv_imgcodecs opencv_video)
 
 if(APPLE)
   find_package(OpenCL REQUIRED)
